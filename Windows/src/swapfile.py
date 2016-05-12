98a14f395c3c67b189b4b35b88de7a8dc29cb9f6
Derek Prince <ace.prince08@gmail.com>
2016-05-12 15:14:05 -0600

    Need to do logging

--- Windows/src/swapfile.py
+++ Windows/src/swapfile.py
@@ -1,8 +1,7 @@
 # Derek Prince
 # May 2016
 import io
-# import os
-import shutil # Used in favor of os because it will make+delete the file as opposed to rename/move for directories not shared on a single disk. os will fail.
+import os, shutil # shutil is used because it plays nice with moving between disks
 import sys # command line arguments. Options, really.
 # getopt vs argparse?
 # argparse is probably a better choice but I prefer C so I might just stick with getopt
@@ -14,33 +13,38 @@ def main():
     parser.add_option("-f", "--file", action="store", type="string", dest="input_file", default="", help="name of file to swap", metavar="FILE")
     parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default="True", help="Suppresses output") # might add ! for no logging
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default="True", help="Prints output")
+    parser.add_option("--install_path", action="store", type="string", dest="install_loc", default="", help="set non-default SteamVR skin path", metavar="INSTALLDIR")
+    parser.addoption("--source_path", action="store", type="string", dest="source_loc", default="",help="set non-default Skins folder path", metavar="SOURCEDIR")
     def restore(option, opt_str, value, parser):
         return
-    parser.add_option("-r", "--restore", action="callback", callback=restore, help="restores default skin file") # keep copy or keep track of theirs? Would require checking .ref
+    parser.add_option("--restore", action="callback", callback=restore, help="restores default skin file") # keep copy or keep track of theirs? Would require checking .ref
     # a benefit of keeping track of .ref is that any Steam updates would be accounted for implicitly
+    def clear(option, opt_str, value, parser):
+        return
+    parser.add_option("--clear", action="callback", callback=clear, help="clears the log file")
     [...]
     # More than two required options is probably a pain in the ass. How to handle locations?
     # Default locations?
-    # ../<skin>?
-    # /dir/path/to/std/output/<outfile>?
+    # ../skins/<new_texture.PNG>?
+    # "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\SteamVR\\resources\\rendermodels\\vr_controller_vive_1_5\\onepointfive_texture.PNG
     # outfile will always be the same since SteamVR doesn't actually support different skins and looks for one name.
     (options, args) = parser.parse_args()
-    if len(args) < 1
-        if options.input_file == "":
-            parser.error("No input file found")
-        else
-            parser.error("No arguments supplied")
+    if len(args) < 1:
+        parser.error("No arguments supplied")
     [...]
+    # start magic
+    if options.file != "":
+        log = open(".log", "a")
+        reference = open(".ref", "r+")
+        current_texture = reference.read()
+        srcdir = os.getcwd() + '\\skins\\'
+        if options.source_loc == "":
+            options.source_loc = srcdir + options.file
+        else:
+            options.source_loc += '\\' + options.file # this is a bit static and dumb for my tastes but I'd rather have a functioning program to work with
+        if options.install_loc == "":
+            options.install_loc = 'C:\\Program Files (x86)\\Steam\\SteamApps\\common\\SteamVR\\resources\\rendermodels\\vr_controller_vive_1_5\\onepointfive_texture.PNG'
+
 
 if __name__ == "__main__":
     main()
-# CL parse
-# check locations valid
-# handle errors
-# open the .ref file if necessary
-# move shit
-# update .ref
-# close .ref
-# echo completion?
-# log?
-# log. (append, don't overwrite. Maybe add an option to refresh logs for the crazy people. Then again they can just delete the file, another one will be created anyhow.)
