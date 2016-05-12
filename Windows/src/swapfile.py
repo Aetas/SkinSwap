5bfdfba1795ad6a0546a12506fd46fadf77528dd
Derek Prince <ace.prince08@gmail.com>
2016-05-11 19:05:10 -0600

    Good work, Skeleton

--- Windows/src/swapfile.py
+++ Windows/src/swapfile.py
@@ -1,4 +1,20 @@
 # Derek Prince
 
 import io
-import os
+# import os
+import shutil # Used in favor of os because it will make+delete the file as opposed to rename/move for directories not shared on a single disk. os will fail.
+import sys, getopt # command line arguments. Options, really.
+# getopt vs argparse?
+# argparse is probably a better choice but I prefer C so I might just stick with getopt
+
+
+# CL parse
+# check locations valid
+# handle errors
+# open the .ref file if necessary
+# move shit
+# update .ref
+# close .ref
+# echo completion?
+# log?
+# log. (append, don't overwrite)
